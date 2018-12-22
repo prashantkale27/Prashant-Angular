a3e34c9b9eeeed0c96984fdaa4844488576f070f
prashantkale27 <prashantkalepatil9@gmail.com>
2018-12-22 13:48:48 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,31 @@ import { RegisterUtdComponent } from './register-utd/register-utd.component';
 import { RegisterUrfComponent } from './register-urf/register-urf.component';
 import { PipesComponent } from './pipes/pipes.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule,Routes  } from '@angular/router'
+import { Route } from '@angular/compiler/src/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+const route:Routes =[
+// { path:"Home", component:HomeComponent},
+
+{ path:"Home", component:HomeComponent} , // default routing if no path is set to route path configuaration object
+{ path:"", redirectTo:'Home',pathMatch:'full'},
+{ path:"Content", component:ContentComponent},
+{ path:"signup", component:SignupComponent,children:[
+  { path:"UTD", component:RegisterUtdComponent},
+  { path:"URF", component:RegisterUrfComponent}
+
+]},
+
+
+{ path:"Pipes", component:PipesComponent},
+{ path:"**", component:PageNotFoundComponent} // wild card routing , if path is not found then it will redirect to page not found component
+
+
+]
 
 @NgModule({
   declarations: [
@@ -22,11 +47,15 @@ import { NavbarComponent } from './navbar/navbar.component';
     RegisterUtdComponent,
     RegisterUrfComponent,
     PipesComponent,
-    NavbarComponent
+    NavbarComponent,
+    PageNotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot(), // External libarary from ngx libarbary
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
